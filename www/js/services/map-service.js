fba3bfb68717665cf0e917138fe11e1cef7b62a1
Adrien Barreau <adrien@mbp-de-adrien.home>
2014-09-14 01:50:28 +0200

    commit map-service

---
+++ www/js/services/map-service.js
@@ -0,0 +1,55 @@
+app.factory("MapService", ["$firebase","$rootScope", function($firebase,$rootScope) {
+
+
+    var latitude = 0;
+    var longitude = 0;
+
+    function construct(latitude, longitude) {
+        latitude = 48.212210;
+        longitude = -1.551944;
+    }
+
+    function init(){
+        construct();
+        var map = {
+            center: {
+                latitude: latitude,
+                longitude: longitude
+            },
+            zoom: 17
+        };
+        var onSuccess = function(position) {
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            map.center = {
+                latitude: latitude,
+                longitude: longitude
+            };
+        }
+        function onError(error) {
+            console.log('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+        }
+        navigator.geolocation.watchPosition(onSuccess, onError, {timeout:10000});
+        return map;
+    }
+
+    function getCurrentPosition() {
+        return {latitude: latitude, longitude: longitude}
+    }
+
+    function createMarker() {
+        marker = {
+            id:0,
+            coords: {
+                latitude: latitude,
+                longitude: longitude
+            }
+        }
+        return marker;
+    }
+
+    return{
+        init : init,
+        createMarker : createMarker
+    };
+}]);
